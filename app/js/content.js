7adf9404bef44ea0bc2b046c9a6f4314451188f6
sayhellotovanessa <vanessa.guedes@britny.se>
2017-02-12 09:57:43 +0100

    fixed content paragraph width

--- app/js/content.js
+++ app/js/content.js
@@ -1,4 +1,10 @@
 var quotes = [
   'The mystery of life is not a problem to solve, but a reality to experience.',
-  'What do you despise? By this are you truly known.'
+  'What do you despise? By this are you truly known.',
+  'Too much knowledge never makes for simple decisions.',
+  'If you rely only on your eyes, your other senses weaken.',
+  'Fear is the mind-killer. Fear is the little death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. ',
+  'Juries are not popular with legalists. Juries oppose the law. They can ignore judges.',
+  'Humans live best when each has his own place, when each knows where he belongs in the scheme of things. Destroy the place and you destroy the person.',
+  'The way the mind will lean under stress is strongly influenced by training.'
 ]
